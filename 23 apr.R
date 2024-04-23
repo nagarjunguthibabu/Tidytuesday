97d14b2e356d18eb025231681c183999fbc3a124
nagarjunguthibabu <nagarjunguthibabu@gmail.com>
2024-04-23 15:02:06 +0200

    first_plot

--- 23 apr.R
+++ 23 apr.R
@@ -1,8 +1,33 @@
+library(tidyverse)
 library(tidytuesdayR)
 
-data <- tidytuesdayR::tt_load("2024-04-23")
+theme_set(theme_minimal(
+  base_size = 12, 
+  base_family = "Volvo Antikva"
+))
 
-data <- data$outer_space_objects
+tuesdata <- tidytuesdayR::tt_load("2024-04-23")
 
+data <- tuesdata$outer_space_objects
 
+str(data)
 
+# first plot
+data %>% 
+  count(Entity, wt = num_objects, sort = TRUE) %>% 
+  slice(1:10) %>% 
+  mutate(Entity = fct_reorder(Entity, n)) %>% 
+  ggplot(aes(n, Entity, fill = Entity))  +
+  geom_col() +
+  geom_text(aes(label = n, hjust = -0.05)) +
+  scale_x_continuous(limits = c(0,20000))+
+  theme(
+    legend.position = "none",
+    axis.text.y = element_text(face = "bold"),
+    title = element_text(size = 16)
+  ) +
+  labs(title = "Objects launched into outer space - top 10 entities/countries",
+       x = "# objects launched into outer space",
+       y = element_blank()
+       )
+  
